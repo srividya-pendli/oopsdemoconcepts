f30d00833e608edf50a3a3a7dfbfcbcc4050e667
srividya-pendli <srividyaerukala@gmail.com>
2022-03-29 20:52:14 +0530

    Add classandobject branch and program

---
+++ Oopsdemos/Program.cs
@@ -0,0 +1,37 @@
+﻿using Oopsdemos.classandobject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oopsdemos
+{
+    class Program
+    {
+        public static object PartTime { get; private set; }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to oops");
+            Console.WriteLine("1:classandobject");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    // code block
+                    Fruit apple = new Fruit("apple", "red");
+                    apple.FruitDetails();
+                    Fruit orange = new Fruit("orange", "yellow");
+                    orange.FruitDetails();
+                    break;
+                default:
+                    Console.WriteLine("please choose number within range");
+                    break;
+            }
+            Console.ReadLine();
+
+        }
+    
+    }
+}
---
+++ Oopsdemos/classandobject/Fruit.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oopsdemos.classandobject
+{
+    class Fruit
+    {
+        //instance varibles
+        public string name, colour;
+
+        public Fruit(string name, string colour)
+        {
+            this.name = name;
+            this.colour = colour;
+        }
+
+        public void FruitDetails()
+        {
+            Console.WriteLine("Fruit Name:{0} colour:{1}", name, colour);
+        }
+
+    }
+}
